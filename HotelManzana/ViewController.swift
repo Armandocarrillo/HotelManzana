d5727d5fe0d3864ebb228ec53ee85117bb2163ac
Armando Carrillo <ingcompuarmando@gmail.com>
2020-07-17 17:59:14 -0500

    Initial Commit

---
+++ HotelManzana/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HotelManzana
+//
+//  Created by Armando Carrillo on 17/07/20.
+//  Copyright © 2020 Armando Carrillo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
